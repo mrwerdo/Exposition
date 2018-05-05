e9c32f0a3df2fcbcfed804b71f99293bf6d67369
Andrew Thompson <mrwerdo331@me.com>
2018-05-05 17:19:48 +1000

    Initial Commit

---
+++ Exposition/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Exposition
+//
+//  Created by Andrew Thompson on 5/5/18.
+//  Copyright © 2018 Andrew Thompson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Exposition/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Exposition
+//
+//  Created by Andrew Thompson on 5/5/18.
+//  Copyright © 2018 Andrew Thompson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
